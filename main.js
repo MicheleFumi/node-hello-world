17286a628494c7a0ebcb41d86dc6b4fdda44491c
MicheleFumi <michele.fumi96@gmail.com>
2024-10-28 14:46:49 +0100

    add bonus

--- main.js
+++ main.js
@@ -2,4 +2,17 @@ console.log('hello node js');
 
 console.log(process.env.PIN);
 
-console.log(process.argv);
+console.log(process.argv.slice(2));
+
+
+
+
+const adminInput = process.env.PIN
+
+if (adminInput === 'admin') {
+    console.log('Welcome Admin');
+    
+} else{
+    console.log('Access Restricted');
+    
+}

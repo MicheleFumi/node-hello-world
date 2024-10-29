982e8bdc835e3cbfa65bb3f06f27f30411c48697
MicheleFumi <michele.fumi96@gmail.com>
2024-10-29 09:48:21 +0100

    bug fix

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ console.log(process.env.PIN);
 
 console.log(process.argv.slice(2));
 
+const adminInput = process.argv[2]
 
 
+const AdminNumber = process.env.PIN
 
-const adminInput = process.env.PIN
-
-if (adminInput === 'admin') {
+if (adminInput == 'admin' && AdminNumber == 666 ) {
     console.log('Welcome Admin');
     
 } else{
